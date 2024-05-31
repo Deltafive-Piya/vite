bd54aba6fd81ae373a1748696007156c17a47116
Deltafive-Piya <pierstoddard@gmail.com>
2024-05-30 22:42:05 -0400

    Change Greeting

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <div>
         <h1 className="text-3xl font-bold underline">
-          Hello world!
+          Hello Vite and Tailwind
         </h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
