9696c42c0a278b6e5014d9846b41b1af1c48c181
Deltafive-Piya <pierstoddard@gmail.com>
2024-06-04 11:17:29 -0400

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <div>
         <h1 className="text-3xl font-bold underline">
-          Hello Vite and Tailwind
+          Hello Vite and Tailwindsss
         </h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
