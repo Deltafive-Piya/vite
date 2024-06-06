fba1dc210aa8f6f0665b6b2587187178b54cf7ff
Deltafive-Piya <pierstoddard@gmail.com>
2024-06-06 13:52:46 -0400

    sidebar comment

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -18,7 +18,7 @@ const SideBar = () => {
 	const scrollToSection = (sectionId) => {
         const section = document.getElementById(sectionId);
         if (section) {
-            const yOffset = -10; // Adjust offset for fixed headers if necessary
+            const yOffset = -10; // offset for fixed headers
             const y = section.getBoundingClientRect().top + window.pageYOffset + yOffset;
 
             window.scrollTo({ top: y, behavior: 'smooth' });
